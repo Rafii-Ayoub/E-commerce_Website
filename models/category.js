09b2d25f31039cf05bd1bc627c9ba8f4cd571f9f
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-11-29 04:30:25 +0100

    reviewing

--- models/category.js
+++ models/category.js
@@ -5,6 +5,14 @@ var Schema = mongoose.Schema;
 var CategorySchema = new Schema({
   title : String,
   description : String,
+  shopID : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref :  "Shop"
+  },
+  createdAt : {
+    type : Date,
+    default : Date.now
+  },
 });
 
 module.exports = mongoose.model('Category', CategorySchema);
